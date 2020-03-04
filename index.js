7089cbee336646766746a0f66091822742c61997
BetoMata <rmata29@alumnos.uaq.mx>
2020-03-04 16:59:09 -0600

    Hola Mundo en Express

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res, next) => {
+    res.send("Bienvenido al servidor");
+})
+
+app.get('/:name', (req, res, next) => {
+    console.log(req.params.name);
+    res.status(200);
+    res.send("Hola " + req.params.name);
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Server is running...");
+});
