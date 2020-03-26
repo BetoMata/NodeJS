088ff1b1d1007362d106b650cdfaf629ce47eace
BetoMata <rmata29@alumnos.uaq.mx>
2020-03-26 01:09:04 -0600

    Uso de archivo pokedex.json

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const app = express();
+const { pokemon } =  require('./pokedex.json');
 
 app.get('/', (req, res, next) => {
-    res.send("Bienvenido al servidor");
+    res.status(200);
+    res.send("Bienvenido al Pokedex");
 })
 
-app.get('/:name', (req, res, next) => {
-    console.log(req.params.name);
+app.get('/pokemon', (req, res, next) => {
     res.status(200);
-    res.send("Hola " + req.params.name);
+    res.send(pokemon);
+});
+
+app.get('/pokemon/:id', (req, res, next) => {
+    res.status(200);
+    res.send(pokemon[req.params.id - 1]);
 });
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running...");
-});
+}); 
 
-//
