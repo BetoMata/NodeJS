e45cc1d1023e60d03fdfd80624056202b128bca8
BetoMata <rmata29@alumnos.uaq.mx>
2020-03-26 02:00:37 -0600

    Obtencion de ciertos datos del archivo JSON por medio de rutas

--- index.js
+++ index.js
@@ -7,14 +7,38 @@ app.get('/', (req, res, next) => {
     res.send("Bienvenido al Pokedex");
 })
 
-app.get('/pokemon', (req, res, next) => {
+app.get('/pokemon/all', (req, res, next) => {
     res.status(200);
     res.send(pokemon);
 });
 
-app.get('/pokemon/:id', (req, res, next) => {
-    res.status(200);
-    res.send(pokemon[req.params.id - 1]);
+app.get('/pokemon/:id([0-9]{1,3})', (req, res, next) => { 
+    const id = req.params.id - 1;
+    if(id >= 0 && id <= 150)
+    {
+        res.status(200);
+        res.send(pokemon[req.params.id - 1]);
+    }
+    else
+    {
+        res.status(404);
+        res.send("Pokemon no encontrado");
+    }
+    
+});
+
+app.get('/pokemon/:name', (req, res, next) => {
+    const name = req.params.name;
+    for(i = 0; i < pokemon.length; i++)
+    {
+        if(pokemon[i].name == name)
+        {
+            res.status(200);
+            res.send(pokemon[i]);
+        }
+    }
+    res.status(404);
+    res.status("Pokemon no encontrado");
 });
 
 app.listen(process.env.PORT || 3000, () => {
