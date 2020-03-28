26826f7c4f4b91e457197702ccca210c161390d9
BetoMata <rmata29@alumnos.uaq.mx>
2020-03-27 22:28:36 -0600

    Cambio de todas las rutas a la base de datos MySQL

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -11,27 +11,26 @@ pokemon.get('/', async (req, res, next) => {
     return res.status(200).json(pkmn);
 });
 
-pokemon.get('/:id([0-9]{1,3})', (req, res, next) => { 
-    const id = req.params.id - 1;
+pokemon.get('/:id([0-9]{1,3})', async (req, res, next) => { 
+    const id = req.params.id;
     if(id >= 0 && id <= 150) 
     {
-        res.status(200).send(pk[req.params.id - 1]);
+        const pkmn = await db.query("SELECT * FROM pokemon WHERE pok_id = ?;", [id]);
+        return res.status(200).json(pkmn);
     } 
-    res.status(404).send("Pokemon no encontrado");
+    return res.status(404).send("Pokemon no encontrado");
 });
 
-pokemon.get('/:name([A-Za-z]+)', (req, res, next) => {
+pokemon.get('/:name([A-Za-z]+)', async (req, res, next) => {
     const name = req.params.name;
 
-    const pkmn = pk.filter((p) => {
-        return (p.name.toUpperCase() == name.toUpperCase()) && p;
-    });
+    const pkmn = await db.query("SELECT * FROM pokemon WHERE pok_name = ?;", [name]);
 
     if(pkmn.length > 0) 
     {
-        res.status(200).send(pkmn);         
+        return res.status(200).json(pkmn);         
     } 
-    res.status(404).send("Pokemon no encontrado");
+    return res.status(404).send("Pokemon no encontrado");
 });
 
 module.exports = pokemon;
