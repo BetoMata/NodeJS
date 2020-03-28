a4fa99c98ff12f1b66a36310664fce0e68804e13
BetoMata <rmata29@alumnos.uaq.mx>
2020-03-27 18:42:10 -0600

    Uso de Router para segmentar el codigo y Morgan para resivir retroalimentacion de la Ruta de acceso

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const app = express();
-const { pokemon } =  require('./pokedex.json');
 
+const pokemon = require('./routes/pokemon');
+
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -10,38 +13,8 @@ app.get("/", (req, res, next) => {
     return res.status(200).send("Bienvenido al Pokedex");
 });
 
-app.post("/pokemon", (req, res, next) => {
-    return res.status(200).send(req.body);
-});
-
-app.get('/pokemon', (req, res, next) => {
-    return res.status(200).send(pokemon);
-});
-
-app.get('/pokemon/:id([0-9]{1,3})', (req, res, next) => { 
-    const id = req.params.id - 1;
-    if(id >= 0 && id <= 150) 
-    {
-        res.status(200).send(pokemon[req.params.id - 1]);
-    } 
-    res.status(404).send("Pokemon no encontrado");
-});
-
-app.get('/pokemon/:name([A-Za-z]+)', (req, res, next) => {
-    const name = req.params.name;
-
-    const pk = pokemon.filter((p) => {
-        return (p.name.toUpperCase() == name.toUpperCase()) && pk;
-    });
-
-    if(pk.length > 0) 
-    {
-        res.status(200).send(pk);         
-    } 
-    res.status(404).send("Pokemon no encontrado");
-});
+app.use("/pokemon", pokemon);
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running...");
 }); 
-
---
+++ routes/pokemon.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const pokemon = express.Router();
+const pk =  require('../pokedex.json').pokemon;
+
+pokemon.post("/", (req, res, next) => {
+    return res.status(200).send(req.body);
+});
+
+pokemon.get('/', (req, res, next) => {
+    return res.status(200).send(pk);
+});
+
+pokemon.get('/:id([0-9]{1,3})', (req, res, next) => { 
+    const id = req.params.id - 1;
+    if(id >= 0 && id <= 150) 
+    {
+        res.status(200).send(pk[req.params.id - 1]);
+    } 
+    res.status(404).send("Pokemon no encontrado");
+});
+
+pokemon.get('/:name([A-Za-z]+)', (req, res, next) => {
+    const name = req.params.name;
+
+    const pkmn = pk.filter((p) => {
+        return (p.name.toUpperCase() == name.toUpperCase()) && p;
+    });
+
+    if(pkmn.length > 0) 
+    {
+        res.status(200).send(pkmn);         
+    } 
+    res.status(404).send("Pokemon no encontrado");
+});
+
+module.exports = pokemon;
