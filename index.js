0f720e4dd6616f75335a03faff9cc8f52a848c58
BetoMata <61714982+BetoMata@users.noreply.github.com>
2020-04-30 18:20:09 -0500

    Creacion de el recurso User

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const morgan = require('morgan');
 const app = express();
 
 const pokemon = require('./routes/pokemon');
+const user = require('./routes/user');
 
 app.use(morgan('dev'));
 app.use(express.json());
@@ -13,6 +14,7 @@ app.get("/", (req, res, next) => {
 });
 
 app.use("/pokemon", pokemon);
+app.use("/user", user);
 
 app.use((req, res, next) => {
     return res.status(404).json({code: 404, message: "URL no encontrada"});
