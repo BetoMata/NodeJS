6aebaeecaebb9ea4ec351d63634ace0844a4398a
BetoMata <61714982+BetoMata@users.noreply.github.com>
2020-05-06 23:35:43 -0500

    Creacion de Login y paquete JWT

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const user = express.Router();
 const db = require('../config/database');
 
@@ -8,7 +9,7 @@ user.post("/", async (req, res, next) => {
     if(user_name && user_mail && user_password)
     {
         let query = "INSERT INTO user(user_name, user_mail, user_password) "
-        query += `VALUES ('${user_name}', '${user_mail}', '${user_password}')`; 
+        query += `VALUES ('${user_name}', '${user_mail}', '${user_password}');`; 
     
         const rows = await db.query(query);
     
@@ -19,6 +20,30 @@ user.post("/", async (req, res, next) => {
         return res.status(500).json({ code: 500, message: "Ocurrio un error"});
     }
     return res.status(500).json({ code: 500, message: "Campos incompletos"});
+});
+
+user.post("/login", async(req, res, next) => {
+    const { user_mail, user_password } = req.body;
+    const query = `SELECT * FROM user WHERE user_mail = '${user_mail}' AND user_password = '${user_password}';`;
+    const rows = await db.query(query);
+    
+    if (user_mail && user_password)
+    {
+        if(rows.length == 1)
+        {
+            const token = jwt.sign({
+                user_id: rows[0].user_id,
+                user_mail: rows[0].user_mail
+            }, "debugkey");
+            return res.status(200).json({ code: 200, message: token });
+        }
+        else
+        {
+            return res.status(401).json({ code: 401, message: "Usuario y/o password incorrectos"});
+        }
+    }
+   
+    return res.status(500).json({ code: 500, message: "Campos incompletos"}); 
 });
 
 user.get("/", async(req, res, next) => {
