c4c5527adf60d814975ed0aa2aadce87870b1c91
BetoMata <61714982+BetoMata@users.noreply.github.com>
2020-05-21 00:02:14 -0500

    Cambio en auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
     try
     {
-        const token = req.headers.authorsization.split(" ")[1];
+        const token = req.headers.authorization.split(" ")[1];
         const decoded = jwt.verify(token, "debugkey");
         req.user = decoded;
         next();
